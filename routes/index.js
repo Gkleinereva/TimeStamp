bf22d10301aba3bd837fd711770aedf1e6c48085
Evan Kleiner <gkleinereva@gmail.com>
2018-05-14 14:29:47 -0700

    Basic functionality working

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
 
 /* GET home page. */
+router.get('/:date', function(req, res, next) {
+	if(!moment(req.params.date).isValid()) {
+		res.json({"unix":null, "natural":null});
+	}
+	else{
+  		var date = moment(req.params.date);
+  		var dateObj = {};
+  		dateObj.unix = date.unix();
+  		dateObj.natural = date.format('MMMM D, YYYY');
+  		res.json(dateObj);
+  	}
+});
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', {title: 'Timestamp Microservice'});
 });
 
 module.exports = router;
